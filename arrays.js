416ccd1968f930c9dbf424b29167bd9cc86f0bab
GUJUGIT <next39742@edu.nextkbh.dk>
2026-02-03 12:26:38 +0100

    opgave 1

--- arrays.js
+++ arrays.js
@@ -4,5 +4,12 @@ let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
 let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
 // --- Løs opgaverne herunder --- 
+// Opgave 1
 
+let res=numbersArray.filter((thisNumber)=>{
+    return thisNumber > 20
+} )
+console.log(res);
+
+// opgave 2
 
